7e3b78a2ff30c79cb8e02ce2ae19963032b66a0a
Deepak Kumar <94438204+Deepak931014@users.noreply.github.com>
2025-02-09 20:21:59 +0530

    Update Gallery.jsx

--- src/components/home/Gallery.jsx
+++ src/components/home/Gallery.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Gallery = () => {
   return (
-    <div className="text-center">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-4/5 mx-auto">
+    <div className="text-center my-10 px-4 sm:px-6 lg:px-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:px-24 px-6 gap-4 w-full mx-auto">
         {Array.from({ length: 12 }).map((_, index) => (
           <div key={index} className="w-full h-80 bg-gray-200"></div>
         ))}
